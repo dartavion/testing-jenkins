e7dfddc59088b619d0e5c59b6612a38b78542e8e
Richard Ranke <richard_ranke@homedepot.com>
2019-08-01 13:57:13 -0400

    pwd

--- getGitFileList.groovy
+++ getGitFileList.groovy
@@ -3,6 +3,7 @@ import static groovy.io.FileType.FILES
 @NonCPS
 
 def inputParamsString(dir) {
+    println pwd()
     def list = [] as java.lang.Object
     dir.eachFileRecurse(FILES) {
         if(it.name.endsWith('.js')) {
